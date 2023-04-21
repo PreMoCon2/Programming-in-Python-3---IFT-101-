1777baea569dbc5ac17155ce1f4c55cf1a0f1573
PreMoCon2 <126223925+PreMoCon2@users.noreply.github.com>
2023-04-20 22:13:35 -0500

    Create 3.1.2: Reading multiple data types.py

---
+++ 3.1.2: Reading multiple data types.py
@@ -0,0 +1,8 @@
+# Read user input for person_name
+person_name = input()
+
+# Read user input for person_age and convert it to an integer using the int() function
+person_age = int(input())
+
+# Print a message about person_name's age in 5 years
+print('In 5 years', person_name, 'will be', person_age + 5)
