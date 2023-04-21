c64bcdfe0c027d7fa5195fa07d04c7ae5f2f82f3
PreMoCon2 <126223925+PreMoCon2@users.noreply.github.com>
2023-04-20 22:44:41 -0500

    Create 3.8.3: Type conversion: Reading and adding values.py

---
+++ 3.8.3: Type conversion: Reading and adding values.py
@@ -0,0 +1,9 @@
+total_owls = 0
+
+num_owls_A = input() # Take input for the number of owls A
+num_owls_B = input() # Take input for the number of owls B
+
+# Convert the input string values to integers and add them to the total_owls variable
+total_owls = int(num_owls_A) + int(num_owls_B)
+
+print(f'Number of owls: {total_owls}')
