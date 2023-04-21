2fd5c3d0c8aa8f677b7be853ceee5755be6b6b65
PreMoCon2 <126223925+PreMoCon2@users.noreply.github.com>
2023-04-20 22:37:47 -0500

    Create 3.5.1: Modify and add to dictionary.py

---
+++ 3.5.1: Modify and add to dictionary.py
@@ -0,0 +1,12 @@
+car_makers = {'Acura': 'Japan', 'Fiat': 'Egypt'}
+
+# Add the key Tesla with value USA to car_makers
+car_makers['Tesla'] = 'USA'
+
+# Modify the car maker of Fiat to Italy
+car_makers['Fiat'] = 'Italy'
+
+# Print the updated dictionary
+print(f'Acura made in {car_makers["Acura"]}')
+print(f'Fiat made in {car_makers["Fiat"]}')
+print(f'Tesla made in {car_makers["Tesla"]}')
