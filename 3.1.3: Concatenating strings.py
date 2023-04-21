a787dff75a87af4cdf3f4a7a65a1b45f8736e57e
PreMoCon2 <126223925+PreMoCon2@users.noreply.github.com>
2023-04-20 22:15:23 -0500

    Create 3.1.3: Concatenating strings.py

---
+++ 3.1.3: Concatenating strings.py
@@ -0,0 +1,10 @@
+```
+# Read in values for my_city and my_state
+my_city = input()
+my_state = input()
+
+# Assign log_entry with current_time, my_city, and my_state separated by a space
+log_entry = current_time + ' ' + my_city + ' ' + my_state
+
+print(log_entry)
+```
