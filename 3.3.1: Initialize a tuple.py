1ddc7405a13f56c52d68b6982b34239f84938940
PreMoCon2 <126223925+PreMoCon2@users.noreply.github.com>
2023-04-20 22:18:46 -0500

    Create 3.3.1: Initialize a tuple.py

---
+++ 3.3.1: Initialize a tuple.py
@@ -0,0 +1,8 @@
+# Initialize the tuple team_names with the strings 'Rockets', 'Raptors', 'Warriors', and 'Celtics'
+team_names = ('Rockets', 'Raptors', 'Warriors', 'Celtics')
+
+# Access and print the elements of the tuple using index
+print(team_names[0])
+print(team_names[1])
+print(team_names[2])
+print(team_names[3])
