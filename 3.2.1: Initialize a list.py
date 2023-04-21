259c2c267471f940ac6e14832599f022957db6aa
PreMoCon2 <126223925+PreMoCon2@users.noreply.github.com>
2023-04-20 22:16:50 -0500

    Create 3.2.1: Initialize a list.py

---
+++ 3.2.1: Initialize a list.py
@@ -0,0 +1,7 @@
+# Initialize the list with strings
+short_names = ['Gus', 'Bob', 'Zoe']
+
+# Output the first three elements in the list
+print(short_names[0])
+print(short_names[1])
+print(short_names[2])
